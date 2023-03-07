ead35d45c34ff583f08f1006f11a289d8ec06462
sihyuuun <leesh3597@gmail.com>
2023-03-07 23:29:48 +0900

    2.4 Log In

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { authService } from "fbase";
 import AppRouter from "components/Router";
 
 function App() {
-  const [isLoggedIn, setIsLoggedin] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedin] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedin(true);
+      } else {
+        setIsLoggedin(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  console.log(authService.currentUser);
+  setInterval(() => {
+    console.log(authService.currentUser);
+  }, 2000);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} Twitter </footer>
     </>
   );
